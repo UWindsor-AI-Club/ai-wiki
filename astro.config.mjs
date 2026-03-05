c3a0715f5890b2864e8368787d2d758a1c19e845
Christine Wong <55353492+Lillliant@users.noreply.github.com>
2026-03-05 10:22:19 -0500

    Test set up of sidebar plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,43 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import starlightSidebarTopics from "starlight-sidebar-topics";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://uwindsor-ai-club.github.io/',
-	base: '/ai-wiki',
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  site: "https://uwindsor-ai-club.github.io/",
+  base: "/ai-wiki",
+  integrations: [
+    starlight({
+      title: "My Docs",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/withastro/starlight",
+        },
+      ],
+      plugins: [
+        starlightSidebarTopics([
+          {
+            label: "Guides",
+            link: "guides/example",
+            icon: "open-book",
+            items: ["guides/example", "guides/example2"],
+          },
+          {
+            label: "Reference",
+            link: "reference/example",
+            icon: "information",
+            items: [
+              {
+                label: "Recipes",
+                autogenerate: { directory: "reference" },
+              },
+            ],
+          }
+        ]),
+      ],
+    }),
+  ],
 });
